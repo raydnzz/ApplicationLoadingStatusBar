5e81c7291c047a4dd0a7ecd7d13720bf2fd27a10
raydnzz <142080547+raydnzz@users.noreply.github.com>
2023-09-29 02:58:41 +0000

    Update MyApplication.kt

--- app/src/main/java/com/duongvn/loadingapplication/MyApplication.kt
+++ app/src/main/java/com/duongvn/loadingapplication/MyApplication.kt
@@ -3,6 +3,7 @@ package com.duongvn.loadingapplication
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.os.Build
 
 
 class MyApplication : Application() {
@@ -17,12 +18,15 @@ class MyApplication : Application() {
     }
 
     private fun createNotificationChannel() {
-        val name  = "Channel DownLoad"
-        val description = "Channel DownLoad Description"
-        val importance = NotificationManager.IMPORTANCE_MIN
-        val channel = NotificationChannel(CHANNEL_ID, name, importance)
-        channel.description = description
-        val notificationManager = getSystemService(NotificationManager::class.java)
-        notificationManager.createNotificationChannel(channel)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val name  = "Channel DownLoad"
+            val description = "Channel DownLoad Description"
+            val importance = NotificationManager.IMPORTANCE_MIN
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+
     }
 }
